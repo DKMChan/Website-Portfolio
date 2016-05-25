6bca55fa20e4c8c45add4defc39040e2455d0f57
DKMChan <desmondkmchan@gmail.com>
2016-05-25 17:25:24 -0600

    Uploaded Resume to files folder and added scrolling navigation template for the website

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
+
 var port = process.env.PORT || 8080;
 
 app.use('/assets',express.static(__dirname + '/public'));
+
+//when a client connects to the website then render the index page
 app.get('/',function(req,res){
    res.render('index'); 
 });
-
 
 app.set('view engine', 'ejs');
 
