a941fd7a476ebe88af2475a984ffa159e8c3f690
DKMChan <desmondkmchan@gmail.com>
2016-05-25 12:43:28 -0600

    Installed express.js and created index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use('/assets',express.static(__dirname + '/public'));
+app.get('/',function(req,res){
+   res.render('index'); 
+});
+
+
+app.set('view engine', 'ejs');
+
+
+app.listen(port);
