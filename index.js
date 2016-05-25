33bf1d146415eeb6f29f5b221d03a34fa91657f6
DKMChan <desmondkmchan@gmail.com>
2016-05-25 12:55:03 -0600

    Changed listen port to 8080 for testing on Amazon EC2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 app.use('/assets',express.static(__dirname + '/public'));
 app.get('/',function(req,res){
