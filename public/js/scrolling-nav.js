4bb4eb6a3b923fb7faea68b92f5c658ed1cc97c0
Desmond <desmondkmchan@gmail.com>
2016-05-29 17:00:57 -0600

    Fixed viewport issue for the skills section where it would overlap with the Contact page on certain screen resolutions

--- public/js/scrolling-nav.js
+++ public/js/scrolling-nav.js
@@ -17,3 +17,60 @@ $(function() {
         event.preventDefault();
     });
 });
+
+
+//hide and display circuit.svg images based on window size
+$(window).resize(function() {
+   var maxSize = window.matchMedia('(max-width: 1200px)');
+   var minSize = window.matchMedia('(min-width: 768px)');
+   if (maxSize.matches)
+   {
+       if(minSize.matches)
+       {
+           $(".circuit-right").hide();
+       }
+   }
+   else{
+       $(".circuit-right").show();
+   }
+    
+});
+
+$(window).resize(function() {
+var midSize = window.matchMedia('(max-width: 750px)');
+
+if(midSize.matches)
+{
+     $(".circuit-right").show();
+   
+   
+}
+});
+
+//$(function() {
+
+function heightToggle(){
+    var elem   = document.getElementById("#htmlcollapse");
+    if($( "#htmlcollapse" ).hasClass('in'))
+    {
+        $('.skills-section').css("height","120vh");
+        console.log("height is now 120vh");
+        console.log("Window Width" + $(window).width());
+        
+    }
+   else //if ($( "#htmlcollapse" ).css( "aria-expanded", "false" ))
+   {
+       if(($(window).width() < 768 ))
+       {
+            $('.skills-section').css("height","180vh");
+            console.log("height is now 180vh");
+            console.log("Window Width" + $(window).width());
+       }
+       else {
+           $('.skills-section').css("height","150vh");
+           console.log('Window width is not less than 768');
+           console.log("Window Width" + $(window).width());
+       }
+    }
+}
+//});
