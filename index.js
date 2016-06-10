a91f0a82cb86754c92558961c54ebe01dfdc3efa
Desmond <desmondkmchan@gmail.com>
2016-06-09 18:49:19 -0600

    Added favicon.ico and serve-favicon package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 var htmlController = require('./controllers/htmlController');
 var todoAppController = require('./controllers/todoAppController');
 var apiController = require('./controllers/apiController');
@@ -9,7 +10,7 @@ var app = express();
 var port = 8080;
 
 app.use('/assets',express.static(__dirname + '/public'));
-
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.set('view engine', 'ejs');
 
 htmlController(app);
